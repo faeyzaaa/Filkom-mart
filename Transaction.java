1e1999b657989d1767bc959f70649a23afcead84
faeyzaaa <faeyzasafaid@student.ub.ac.id>
2026-03-14 14:13:47 +0700

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -3,14 +3,39 @@ public class Transaction {
     private Product[] items;
     private int totalItems;
 
-    public double processSale(){
-        return 0;
+    public Transaction(String transactionId) {
+        this.transactionId = transactionId;
+        this.items = new Product[50]; 
+        this.totalItems = 0;
     }
-    public void addItem(Product item){
 
+    public double processSale(){
+        double total = 0;
+
+        for(int i = 0; i < totalItems; i++){
+            Product p = items[i];
+
+            double discount = p.calculateDiscount();
+            double finalPrice = p.getPrice() - (p.getPrice() * discount);
+
+            total += finalPrice;
+        }
+
+        return total;
+    }
+
+    public void addItem(Product item){
+        if(totalItems < items.length){
+            items[totalItems] = item;
+            totalItems++;
+        }else{
+            System.out.println("Keranjang penuh");
+        }
     }
 
     public void addItem(Product item, int quantity){
-
+        for(int i = 0; i < quantity; i++){
+            addItem(item);
+        }
     }
 }
